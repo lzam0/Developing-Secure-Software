bf080114e8d52fc0243d6dba843b357acd8e9aa5
psh22xtu <itsleihl@gmail.com>
2026-03-12 11:23:18 +0000

    Header Component

---
+++ client/components/header.js
@@ -0,0 +1,13 @@
+async function loadHeader(){
+
+const response = await fetch("/components/header.html");
+
+const data = await response.text();
+
+document
+.getElementById("header")
+.innerHTML = data;
+
+}
+
+loadHeader();
