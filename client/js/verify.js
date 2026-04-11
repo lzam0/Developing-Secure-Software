b7a94c77cef85b3621906cd3336a9e78cd0faaa1
lzam <110193626+lzam0@users.noreply.github.com>
2026-04-11 09:18:22 +0100

    Logout functionality and check if current session is logged in
    Verify.js located on the front end, it is called in the html header.
    Sends a backend request and calls /auth/verify. If the current session is valid, then keep the user logged in.
    
    Users can now log out. Button is hidden until the checks from the backend verify that a current session is ongoing.
    
    Updated Read Me Document

---
+++ client/js/verify.js
@@ -0,0 +1,17 @@
+// Reusable auth verification utility
+// Must be loaded before auth-guard.js and header.js
+
+const BACKEND_URL = "http://localhost:5000";
+
+
+async function verifyAuth() {
+    // try to verify the auth by sending a request to the backend
+    try {
+        // API call to auth/verify route
+        const res = await fetch(`${BACKEND_URL}/auth/verify`, { credentials: "include" });
+        return res.ok;
+    } catch {
+        // if the request fails, return false§
+        return false;
+    }
+}
