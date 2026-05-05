8349df0b551bbef846f7d5d4a7d5bdb9fb3726c0
dylayoun <16dylayoun@gmail.com>
2026-05-05 14:05:02 +0100

    CSRF frontend/backend additional adds
    added validate to signout route and created it in auth.routes.js
    added validateCSRF to login and signup to allow for creation of tokens to be added to cookie header as X-CSRF-Token

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import authController from '../controllers/auth.controller.js';
 import { validateSignUp, validateSignIn} from '../middleware/validation.middleware.js';
+import { validateCSRF } from '../middleware/csrf.middleware.js';
 import authenticateToken from '../middleware/auth.middleware.js';
 import rateLimit from 'express-rate-limit';
 const router = Router();
@@ -17,6 +18,7 @@ const authLimiter = rateLimit({
 router.post('/signUp', authLimiter, validateSignUp, authController.signUp.bind(authController));
 router.post('/signIn', authLimiter, validateSignIn, authController.signIn.bind(authController));
 router.post('/login', authLimiter, validateSignIn, authController.signIn.bind(authController));
+router.post('/signOut', authenticateToken, validateCSRF, authController.signOut.bind(authController));
 
 
 /* Protected Routes */
