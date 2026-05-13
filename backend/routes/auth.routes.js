c91e24f328b36632b0bda44dad7c74f64ddcd3df
lzam <110193626+lzam0@users.noreply.github.com>
2026-05-13 23:24:06 +0100

    auth rate limiting

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,10 +7,10 @@ import { requirePendingOtpSession } from '../middleware/otp-session.middleware.j
 import rateLimit from 'express-rate-limit';
 const router = Router();
 
-//auth limiting
+//auth rate limiting
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10, // limit each IP to 10 requests per windowMs
+    max: 20, // limit each IP to 20 requests per windowMs
     message: 'Too many authentication attempts from your IP, please try again after 15 minutes'
 });
 
